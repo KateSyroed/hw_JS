c6f88dbb69b4fea27b0b759ffd763ca11b29028a
QA_KateSyroied <katesyroed@gmail.com>
2023-11-04 13:26:06 +0200

    HW_3

---
+++ HW3.js
@@ -0,0 +1,51 @@
+// Task - 1
+let planetName = 'Earth';
+console.log(planetName);
+
+// Task - 2
+let visitorName = 'John';
+console.log(visitorName);
+
+// Task - 3
+var myAge = 30;
+console.log(myAge);
+
+// Task - 4
+let myName = 'Alice';
+console.log(myName);
+
+// Task - 5
+const myBool = false;
+console.log(myBool);
+
+// Task - 6
+let x = 15;
+let y = 30;
+console.log(x + y);
+
+// Task - 7
+let myUndefined = undefined;
+console.log(myUndefined);
+
+// Task - 8
+let dividedNumber = myAge / 2;
+console.log(dividedNumber);
+
+// Task - 9
+let multipleNumber = myAge * 2;
+console.log(multipleNumber);
+
+// Task - 10
+let mySentence = myName + ' is ' + myAge;
+console.log(mySentence);
+
+// Task - 11
+let myUndefined2;
+myUndefined2 = undefined;
+console.log(myUndefined2);
+
+// Task - 12
+let admin, name;
+name = 'Bill Gates';
+admin = name;
+console.log(admin);
